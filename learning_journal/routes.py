f83cb62f0fe8a64b973ee0c2e5b4ce77d92a8d4c
Loaye <jr.carstens00@gmail.com>
2017-10-31 09:22:33 -0700

    adds routes to the view routes file

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -1,3 +1,15 @@
+"""View Routes"""
+from learning_journal.views.default import (
+    list_view,
+    detail_view,
+    create_view,
+    update_view
+)
+
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    config.add_route('home', '/')
+    config.add_route('list_view', '/')
+    config.add_route('detail_view', '/journal/{id:\d+}')
+    config.add_route('create_view', '/journal/new_entry')
+    config.add_route('update_view', '/{id:\d+}/edit_entry')
+    
