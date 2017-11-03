9ed1f13540d44680083e7dbcc8e5bc9d47857c03
Loaye <jr.carstens00@gmail.com>
2017-11-03 14:59:29 -0700

    changes add_route to add_view

--- learning_journal/views/__init__.py
+++ learning_journal/views/__init__.py
@@ -8,7 +8,7 @@ from learning_journal.views.default import (
 
 def includeme(config):
     """Add views for each view and route"""
-    config.add_route('list_view', route_name="home")
-    config.add_route('detail_view', route_name="detail")
-    config.add_route('create_view', route_name="create")
-    config.add_route('update_view', route_name="update")
+    config.add_view(list_view, route_name="home")
+    config.add_view(detail_view, route_name="detail")
+    config.add_view(create_view, route_name="create")
+    config.add_view(update_view, route_name="update")
