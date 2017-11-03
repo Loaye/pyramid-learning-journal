e246c7d93a908deb3f0740a1da5a9a05eea6812a
Loaye <jr.carstens00@gmail.com>
2017-11-02 18:57:45 -0700

    fixes setup.py

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -9,25 +9,25 @@ def list_view(request):
     """Home Page."""
     path = os.path.join(HERE, '../templates/list_view.html')
     with io.open(path) as imported_text:
-        return Response(imported_text.read)
+        return Response(imported_text.read())
 
 
 def create_view(request):
     """Create a new blog post."""
     path = os.path.join(HERE, '../templates/create_view.html')
     with io.open(path) as imported_text:
-        return Response(imported_text.read)
+        return Response(imported_text.read())
 
 
 def detail_view(request):
     """Show single blog post."""
     path = os.path.join(HERE, '../templates/detail_view.html')
     with io.open(path) as imported_text:
-        return Response(imported_text.read)
+        return Response(imported_text.read())
 
 
 def update_view(request):
     """Update an existing blog entry."""
     path = os.path.join(HERE, '../templates/update_view.html')
     with io.open(path) as imported_text:
-        return Response(imported_text.read)
+        return Response(imported_text.read())
