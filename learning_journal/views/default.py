26ca990d69eb6232a04aa2b186195a2f3ff1108b
Loaye <jr.carstens00@gmail.com>
2017-11-04 11:20:39 -0700

    trucking away at step2

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,26 +1,35 @@
 """Set the default for the app."""
 from pyramid.view import view_config
+from learning_journal.data.entry_data import ENTRIES
 
 
 @view_config(route_name="list_view", renderer="../templates/list_view.jinja2")
 def list_view(request):
     """Home Page."""
-    return {}
+    return {
+        "entries": ENTRIES
+    }
 
 
 @view_config(route_name="create_view", renderer="../templates/create_view.jinja2")
 def create_view(request):
     """Create a new blog post."""
-    return {}
+    return {
+        "entries": ENTRIES
+    }
 
 
 @view_config(route_name="detail_view", renderer="../templates/route_view.jinja2")
 def detail_view(request):
     """Show single blog post."""
-    return {}
+    return {
+        "entries": ENTRIES
+    }
 
 
 @view_config(route_name="update_view", renderer="../templates/update_view.jinja2")
 def update_view(request):
     """Update an existing blog entry."""
-    return {}
+    return {
+        "entries": ENTRIES
+    }
