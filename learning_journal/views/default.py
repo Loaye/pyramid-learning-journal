7f07d4b315c040cbbd607b3c1231d4521a34b239
Loaye <jr.carstens00@gmail.com>
2017-11-04 17:29:24 -0700

    adds a detail view and render

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,9 +1,11 @@
 """Set the default for the app."""
 from pyramid.view import view_config
+from datetime import datetime
+from pyramid.httpexceptions import HTTPNotFound
 from learning_journal.data.entry_data import ENTRIES
 
 
-@view_config(route_name="list_view", renderer="../templates/list_view.jinja2")
+@view_config(route_name="home", renderer="../templates/list.jinja2")
 def list_view(request):
     """Home Page."""
     return {
@@ -11,25 +13,26 @@ def list_view(request):
     }
 
 
-@view_config(route_name="create_view", renderer="../templates/create_view.jinja2")
+@view_config(route_name="detail", renderer="../templates/detail.jinja2")
+def detail_view(request):
+    """Show single blog post."""
+    entry_id = int(request.matchdict['id'])
+    for entry in ENTRIES:
+        if entry["id"] == entry_id:
+            return{"entry" : entry }
+
+
+@view_config(route_name="create", renderer="../templates/create.jinja2")
 def create_view(request):
     """Create a new blog post."""
     return {
-        "entries": ENTRIES
+       
     }
 
 
-@view_config(route_name="detail_view", renderer="../templates/route_view.jinja2")
-def detail_view(request):
-    """Show single blog post."""
-    return {
-        "entries": ENTRIES
-    }
-
-
-@view_config(route_name="update_view", renderer="../templates/update_view.jinja2")
+@view_config(route_name="update", renderer="../templates/update.jinja2")
 def update_view(request):
     """Update an existing blog entry."""
     return {
-        "entries": ENTRIES
+        "entries": id
     }
